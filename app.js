2a4cdefdf480948a9c0073bf262d3a9bdaf9baf6
Rivenn-AI <jalolbekatamurodov@gmail.com>
2026-09-01 15:47:30 +0900

    yangi file va Task-B

---
+++ app.js
@@ -0,0 +1,253 @@
+console.log("webserverni boshlash");
+
+const express = require("express");
+
+const app = express();
+
+// MongoDB database server.js dan keladi
+module.exports = function (db) {
+
+  // Static files
+  app.use(express.static("public"));
+
+  // Body parser
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // EJS
+  app.set("views", "views");
+  app.set("view engine", "ejs");
+
+
+  // =========================
+  // YANGI REJA QO'SHISH
+  // =========================
+
+  app.post("/create-item", async (req, res) => {
+      console.log('user entered /create-item');
+
+    console.log(req.body);
+
+    const new_reja = req.body.reja;
+
+    try {
+
+      const result = await db.collection("plan").insertOne({
+        reja: new_reja
+      });
+
+      console.log("MongoDB ga saqlandi:", result);
+
+      res.redirect("/");
+
+    } catch (err) {
+
+      console.log("MongoDB ERROR:", err);
+
+      res.status(500).send("Something went wrong");
+
+    }
+
+  });
+
+
+  // =========================
+  // BARCHA REJALARNI OLISH
+  // =========================
+
+  app.get("/", async (req, res) => {
+           console.log('user entered /');
+    try {
+
+      const data = await db.collection("plan").find().toArray();
+
+      console.log("MongoDB data:", data);
+
+      res.render("reja", {
+        items: data
+      });
+
+    } catch (err) {
+
+      console.log("MongoDB ERROR:", err);
+
+      res.status(500).send("Something went wrong");
+
+    }
+
+  });
+
+
+  return app;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("webserverni boshlash");
+// const express = require("express"); 
+// const app = express(); 
+
+// // Mongo db chaqrish
+// const db = require("./server").db();
+// //const http = require("http"); 
+// // const fs = require("fs");
+// // let user;
+// // fs.readFile("database/user.json","utf-8", (err, data) =>{
+// //          if(err) {
+// //             console.log("ERROR",err);
+// //          } else{
+// //             user = JSON.parse(data); 
+// //          }
+// // });
+// //1 Expressga
+// app.use(express.static("public")); 
+// app.use(express.json()); 
+// app.use(express.urlencoded({extended: true})); 
+// //2 Sessions
+// //3 Backend side server. View code
+// app.set("views", "views");
+// app.set("view engine", "ejs");
+// //4
+// app.post("/create-item", (req, res) =>{
+//   console.log(req.body);
+//   const new_reja = req.body.reja;
+//   db.collection("plans").insertOne( {reja: new_reja}, (err,data) => {
+//     if(err){
+//       console.log(err);
+//       res.end("Something went wrong");
+//     } else{
+//       console.log(data);
+//       res.end("Succesfully added");
+//     }
+//   });
+// });
+
+// app.get("/",function (req,res) {
+//   db.collection("plans")
+//   .find()
+//   .toArray( (err, data) => {
+//     if(err) {
+//       console.log(err);
+//       res.end("Something went wrong");
+//     } else{
+//       console.log(data);
+//       res.render("reja", {items: data});
+//     }
+//   });
+  
+// });
+// module.exports = app;
+// app.get("/", function(req,res){
+//     res.end(`<h1>Hello world by Riven</h1>`);
+// });
+
+// const server =http.createServer(app); 
+// let PORT = 4001;
+// server.listen(PORT, function () {
+//     console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
+// });
+//=========23-24-25==============// Mongo va Crud urnatish
+//Mongo db ulash
+// //====================21-22======================//
+
+// console.log("Ota-bobmizdan qolgan maslahatlar");
+// const list =[
+// "yaxshi talaba buling", //0-20
+// "tugri boshliq tanlang va kuproq xato qiling", //20-30
+// "uzingizga ishlashni boshlang", //30-40
+// "siz kuchli bulgan ishni qiling", //40-50
+// "yoshlarga investitsiya qiling", //50-60
+// "Endi foydasi yuq, damni oling", //60
+// ];
+//   //===========Synchronous function============//
+// // //callback function
+// // function maslahatBering (a, callback){
+// //     if(typeof a !== 'number') callback("Insert number",null);
+// //      else if(a <=20) callback(null, list[0]);
+// //      else if(a >20 && a <=30) callback(null, list[1]);
+// //      else if(a >30 && a <=40) callback(null, list[2]);
+// //      else if(a >40 && a <=50) callback(null, list[3]);
+// //      else if(a >50 && a <=60) callback(null, list[4]);
+// //       else{
+// //         setInterval(function() {
+// //             callback(null,list[5]);
+// //         }, 1000)
+// //       }
+// // }
+  
+      
+// //       // Asiykirness function sinkerness functionlar utib bulgandan keyin asinkirness functionga utadi.
+// // maslahatBering(65, (err, data) => {
+// //     if(err) console.log('ERROR', err);
+// //     else{
+// //         console.log('Javob:', data);
+// //     }
+// // });
+// //  console.log("bu oxri")
+
+
+// // //==========Asynchronous function=========//
+// async function maslahatBering (a,){
+//     if(typeof a !== 'number') throw new Error("Insert number");
+//      else if(a <=20) return list[0];
+//      else if(a >20 && a <=30) return list[1];
+//      else if(a >30 && a <=40) return list[2];
+//      else if(a >40 && a <=50) return list[3];
+//      else if(a >50 && a <=60) return list[4];
+//       else{
+       
+//         return new Promise ((resolve, reject) =>{
+//                   setTimeout( () => {
+//                           resolve( list[5]);
+//                   },1000);
+//                 });
+//         }
+//     }
+    
+    
+// //==========>>>>>Promise ni ichda setTime out ishlaydi
+
+
+// //==========>>>>>>>  THEN. CATCH.
+// // console.log("buyerda utdi");
+// // maslahatBering(65)
+// //   .then((data) => {
+// //     console.log("javob:",data);
+// //   })
+// //   .catch((err) => {
+// //     console.log("ERROR", err);
+// //   });
+// //  console.log("bu oxri") // Asiykirness function sinkerness functionlar utib bulgandan keyin asinkirness functionga utadi.
+
+
+//  //===========>>>>>> ASYNCH. AWAIT
+
+// async function run() {
+//     let javob = await maslahatBering(24);
+//     console.log(javob);
+//     javob = await maslahatBering(67);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob)
+// }
+// run();
--- server.js
+++ server.js
@@ -1,36 +1,151 @@
-console.log("webserverni boshlash");
-const express = require("express"); 
-const app = express(); 
-const http = require("http"); 
-const fs = require("fs");
-let user;
-fs.readFile("database/user.json","utf-8", (err, data) =>{
-         if(err) {
-            console.log("ERROR",err);
-         } else{
-            user = JSON.parse(data); 
-         }
-});
-//1 Expressga
-app.use(express.static("public")); 
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true})); 
-//2 Sessions
-//3 Backend side server. View code
-app.set("views", "views");
-app.set("view engine", "ejs");
-//4
+const http = require("http");
+const { MongoClient } = require("mongodb");
+
+const connectionString =
+  "mongodb+srv://Riven:Jaloliddin2005A@cluster0.mgxxaw2.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(connectionString);
+
+async function startServer() {
+
+  try {
+
+    await client.connect();
+
+    console.log("Mongo db connect success");
+
+    // MongoDB database
+    const db = client.db("Reja");
+
+    // app.js ga db ni beramiz
+    const createApp = require("./app");
+
+    const app = createApp(db);
+
+    const server = http.createServer(app);
+
+    const PORT = 4001;
+
+    server.listen(PORT, () => {
+
+      console.log(
+        `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+      );
+
+    });
+
+  } catch (err) {
+
+    console.log("MongoDB connection error:", err);
+
+  }
+
+}
+
+startServer();
+
+// const http = require("http"); 
+// const mongodb= require("mongodb");
+
+
+// let db;
+// const connectionString = 
+// "mongodb+srv://Riven:Jaloliddin2005A@cluster0.mgxxaw2.mongodb.net/?appName=Cluster0"
+
+
+// mongodb.connect(connectionString,
+//      {useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//      }, 
+//    (err, client) => {
+//     if(err) console.log("ERROR on connection to Mongo db");
+//     else{
+//         console.log("Mongo db connect success")
+//         module.exports = client;
+//         const app = require("./app");
+//         const server =http.createServer(app); 
+//         let PORT = 4001;
+//         server.listen(PORT, function () {
+//        console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
+//      });
+//     };
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("webserverni boshlash");
+// const express = require("express"); 
+// const app = express(); 
+// const http = require("http"); 
+// const fs = require("fs");
+// let user;
+// fs.readFile("database/user.json","utf-8", (err, data) =>{
+//          if(err) {
+//             console.log("ERROR",err);
+//          } else{
+//             user = JSON.parse(data); 
+//          }
+// });
+// //1 Expressga
+// app.use(express.static("public")); 
+// app.use(express.json()); 
+// app.use(express.urlencoded({extended: true})); 
+// //2 Sessions
+// //3 Backend side server. View code
+// app.set("views", "views");
+// app.set("view engine", "ejs");
+// //4
     
 
-app.get("/author",(req,res)=> {
-  res.render("author", {user: user});
-});
-app.get("/", function(req,res){
-    res.end(`<h1>Hello world by Riven</h1>`);
-});
+// app.get("/author",(req,res)=> {
+//   res.render("author", {user: user});
+// });
+// app.get("/", function(req,res){
+//     res.end(`<h1>Hello world by Riven</h1>`);
+// });
 
-const server =http.createServer(app); 
-let PORT = 4000;
-server.listen(PORT, function () {
-    console.log(`The server is running succesfully on port: ${PORT}`);
-});
+// const server =http.createServer(app); 
+// let PORT = 4000;
+// server.listen(PORT, function () {
+//     console.log(`The server is running succesfully on port: ${PORT}`);
+// });
