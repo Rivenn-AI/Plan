0c3321008833f2d37f2adf55d101c68da4618297
Rivenn-AI <jalolbekatamurodov@gmail.com>
2026-09-09 22:24:50 +0900

    Task-c

--- train.js
+++ train.js
@@ -1,57 +1,66 @@
-
+//MIT task-c
+function teskari(str) {
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    //for (boshlanish; shart; o'zgarish)
+    reverse += str[i];
+  }
+  return reverse;
+}
+console.log(teskari("Ali"));
 //MIT task-d
-//Shunday class tuzing tuzing nomi Shop, 
-// va uni constructoriga 3 hil mahsulot pass 
-// bolsin, hamda classning 3ta methodi bolsin, 
-// biri qoldiq, biri sotish va biri qabul. 
-// Har bir method ishga tushgan vaqt ham log qilinsin. 
-// MASALAN: const shop = new Shop(4, 5, 2); 
-// shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! 
-// shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() 
+//Shunday class tuzing tuzing nomi Shop,
+// va uni constructoriga 3 hil mahsulot pass
+// bolsin, hamda classning 3ta methodi bolsin,
+// biri qoldiq, biri sotish va biri qabul.
+// Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2);
+// shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud!
+// shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq()
 // return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
-class Shop{
-  #non;
-  #cola;
-   #lagmon;
-  constructor(non, cola, lagmon){
-    this.#non = non;
-    this.#cola= cola;
-    this.#lagmon= lagmon;
-  }
-  
-  qoldiq(){
-    const soldTime = new Date().toLocaleTimeString();
-    console.log(`Hozir soat ${soldTime} da ${this.#non} ta non, ${this.#cola} ta cola va ${this.#lagmon} ta lagmon bor`)
-  }
-  sotish(maxsulot, qiymat) {
-    if (maxsulot === "non") {
-        this.#non -= qiymat;
-    } else if (maxsulot === "cola") {
-        this.#cola -= qiymat;
-    } else if (maxsulot === "lagmon") {
-        this.#lagmon -= qiymat;
-    }
+// class Shop{
+//   #non;
+//   #cola;
+//    #lagmon;
+//   constructor(non, cola, lagmon){
+//     this.#non = non;
+//     this.#cola= cola;
+//     this.#lagmon= lagmon;
+//   }
 
-    console.log(`Sotilgan tavar: ${maxsulot}, miqdori: ${qiymat}`);
-}
- qabul(maxsulot, qiymat){
-  if (maxsulot === "non") {
-    this.#non += qiymat;
-} else if (maxsulot === "cola") {
-    this.#cola += qiymat;
-} else if (maxsulot === "lagmon") {
-    this.#lagmon += qiymat;
-}
-  }
-}
-const myShop = new Shop(4,5,2);
-myShop.qoldiq();
-myShop.sotish("non",1);
-myShop.qoldiq();
-myShop.qabul("lagmon",5);
-myShop.qoldiq();
-myShop.sotish("cola",2)
-myShop.qoldiq();
+//   qoldiq(){
+//     const soldTime = new Date().toLocaleTimeString();
+//     console.log(`Hozir soat ${soldTime} da ${this.#non} ta non, ${this.#cola} ta cola va ${this.#lagmon} ta lagmon bor`)
+//   }
+//   sotish(maxsulot, qiymat) {
+//     if (maxsulot === "non") {
+//         this.#non -= qiymat;
+//     } else if (maxsulot === "cola") {
+//         this.#cola -= qiymat;
+//     } else if (maxsulot === "lagmon") {
+//         this.#lagmon -= qiymat;
+//     }
+
+//     console.log(`Sotilgan tavar: ${maxsulot}, miqdori: ${qiymat}`);
+// }
+//  qabul(maxsulot, qiymat){
+//   if (maxsulot === "non") {
+//     this.#non += qiymat;
+// } else if (maxsulot === "cola") {
+//     this.#cola += qiymat;
+// } else if (maxsulot === "lagmon") {
+//     this.#lagmon += qiymat;
+// }
+//   }
+// }
+// const myShop = new Shop(4,5,2);
+// myShop.qoldiq();
+// myShop.sotish("non",1);
+// myShop.qoldiq();
+// myShop.qabul("lagmon",5);
+// myShop.qoldiq();
+// myShop.sotish("cola",2)
+// myShop.qoldiq();
 //MIT task-C
 // function check(a,b){
 //   return a.split("").sort().join("") === b.split("").sort().join("");
@@ -78,8 +87,6 @@ myShop.qoldiq();
 //             count++
 //         }
 //     }
-//        return count; 
+//        return count;
 // };
 // console.log(countLetter("d","Muhriddin"));
-
-
